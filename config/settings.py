8fd2925dd15dc1c4d5287496676a7acc25313c14
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2024-11-01 02:14:08 -0300

    configurando host

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Configurações do Django
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=True) 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1']) 
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1', 'leonardoitalo20.pythonanywhere.com']) 
 
 
 # Application definition
